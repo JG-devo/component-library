7279d9d3a29098d1e18de127c937afc7fbb5fb47
Jean Gabriel <67287800+JG-devo@users.noreply.github.com>
2022-08-01 18:21:36 +0100

    initial try building progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,64 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  large: {
+    '--padding': 4 + 'px',
+    '--height': 24 + 'px',
+  },
+  medium: {
+    '--padding': 0 + 'px',
+    '--height': 12 + 'px',
+  },
+  small: {
+    '--padding': 0 + 'px',
+    '--height': 8 + 'px',
+  },
 };
+
+const ProgressBar = ({ value, size }) => {
+  const styles = SIZES[size];
+
+  return (
+    <Wrapper
+      style={styles}
+      role='meter'
+      aria-valuenow={value}
+      aria-valuemin='0'
+      aria-valuemax='100'
+    >
+      <Bar
+        width={value}
+        aria-hidden='true'
+        version='1.1'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <rect
+          x='0'
+          y='0'
+          width='100%'
+          height='100%'
+          fill={COLORS.primary}
+        ></rect>
+      </Bar>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  background: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  width: 200px;
+  height: var(--height);
+  padding: var(--padding);
+  border-radius: 8px;
+`;
+
+const Bar = styled.svg`
+  width: ${props => props.width + `%`};
+  height: 100%;
+  box-sizing: border-box;
+  border-radius: 4px 0px 0px 4px;
+`;
 
 export default ProgressBar;
